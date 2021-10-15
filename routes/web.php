7e3d6440913117142bf1cebfce23ff8972834ba1
福田 <ryuuga>
2021-10-15 02:48:26 +0000

    コミットメッセージ

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,17 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('news/delete', 'Admin\NewsController@delete');
-    Route::get('news/update', 'Admin\NewsController@update');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    Route::get('news/update', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     
 });
 
 
+
  Route::get('XXX', 'AAAController@bbb');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
